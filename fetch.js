934aaad485c54d034813c889be66c081a1f6bd89
Naitik770 <prajapatinaitik2435@gmail.com>
2026-04-02 18:48:50 +0530

    feat: Add basic HTTPS fetch examples
    Introduces new JavaScript and TypeScript files demonstrating how to make HTTPS requests to a specified URL. These examples serve as foundational code for future network operations within the application.

---
+++ fetch.js
@@ -0,0 +1,10 @@
+import https from 'https';
+
+https.get('https://neurixapp.netlify.app', (res) => {
+  console.log('Status Code:', res.statusCode);
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+}).on('error', (e) => {
+  console.error(e);
+});
